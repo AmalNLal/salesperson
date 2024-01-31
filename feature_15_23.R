1613ae2a9ff44ddf770fdf7e06b4def91046e7d0
Ajay Umakanth <ajayunp@gmail.com>
2024-01-31 22:06:05 +0100

    movved fe<ture extraction code to a common file

---
+++ feature_15_23.R
@@ -0,0 +1,87 @@
+# Load necessary library for skewness
+library(e1071)
+library(TSP)
+
+calculate_stats <- function(arr) {
+  # Calculate mean
+  mean_value <- mean(arr, na.rm = TRUE)
+  
+  # Calculate standard deviation
+  sd_value <- sd(arr, na.rm = TRUE)
+  
+  # Calculate coefficient of variation
+  # Note: Coefficient of Variation = SD / Mean
+  cv_value <- sd_value / mean_value
+  
+  # Calculate skewness
+  skew_value <- skewness(arr, type = 3, na.rm = TRUE)
+  
+  # Return the results as an array
+  return(c(mean_value, cv_value, skew_value))
+}
+
+getTourCost <- function(dataset_path) {
+  solutionlk <- solve_TSP(dataset_path, method = "linkern")
+  return(tour_length(solutionlk))
+}
+
+getMinTourLength <- function(dataset_path, linkern_path) {
+  results <- system2(linkern_path, args = c(dataset_path), stdout = TRUE, stderr=TRUE)
+  local_min_tour <- NULL
+  for (line in results) {
+    if (grepl("Best cycle length: (\\d+).*", line)) {
+      local_min_tour <- as.numeric(gsub("[^0-9]", "", line))
+    }
+  }
+  return(local_min_tour)
+}
+
+getImpPerStep <- function(dataset_path, linkern_path) {
+  results <- system2(linkern_path, args = c(dataset_path), stdout = TRUE, stderr=TRUE)
+  results
+  steps <- NULL
+  costs <- NULL
+  best_sol <- NULL
+  imp_per_step <- NULL
+  for (line in results) {
+    if (grepl("\\s+(\\d+) Steps.*Best:", line)) {
+      steps <- c(steps, as.numeric(gsub("\\s+(\\d+) Steps.*Best:.*", "\\1", line)))
+      costs <- c(costs, as.numeric(gsub(".*Best: (\\d+).*", "\\1", line)))
+    }
+    if (grepl("Best cycle length: (\\d+).*", line)) {
+      best_sol <- as.numeric(gsub("[^0-9]", "", line))
+    }
+  }
+  for (i in 1:length(costs)) {
+    if (best_sol == costs[i]) {
+      imp_per_step <- (costs[1] - best_sol) / steps[i]
+      break
+    }
+  }
+  return(imp_per_step)
+}
+
+feature_15_23<-function(dataset_path,linkern_path){
+  tour_costs<-c()
+  min_tour_lengths<-c()
+  impPerStep<-c()
+  for (i in 1:20){
+    # Call the function and store the result
+    tour_costs <- c(tour_costs, getTourCost(dataset_path))
+    min_tour_lengths <- c(min_tour_lengths, getMinTourLength(dataset_path,linkern_path))
+    impPerStep <- c(impPerStep, getImpPerStep(dataset_path,linkern_path))
+  }
+  feature_15_17<-calculate_stats(tour_costs)
+  feature_18_20<-calculate_stats(min_tour_lengths)
+  feature_21_23<-calculate_stats(impPerStep)
+  
+  return(c(feature_15_17,feature_18_20,feature_21_23))
+}
+dataset_path <- "/home/ajayum/Documents/seminar/ALL_tsp/a280.tsp"
+linkern_path <- "/home/ajayum/Documents/seminar/concorde/TSP/linkern"
+con_path <- "/home/ajayum/Documents/seminar/concorde/TSP"
+concorde_path(con_path)
+
+getTourCost(dataset_path)
+
+feature_15_23(dataset_path, linkern_path)
